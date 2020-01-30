a94eab640cae55ddbd7c1be6c70bcba75de85761
StylishTriangles <hungrycow2012@gmail.com>
2020-01-30 22:57:25 +0100

    Add a surprise :)

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -2,3 +2,8 @@ function hello() {
     let x = "doggo";
     console.log("Hello "+x+"!");
 }
+
+function see_image() {
+    img = document.getElementById("ksp");
+    img.removeAttribute("hidden")
+}
