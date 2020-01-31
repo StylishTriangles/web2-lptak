f865134eb222b2fd99f89f4eed658cf67674bd14
StylishTriangles <hungrycow2012@gmail.com>
2020-01-31 01:13:05 +0100

    Button fun

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -6,4 +6,17 @@ function hello() {
 function see_image() {
     img = document.getElementById("ksp");
     img.removeAttribute("hidden")
+    greeting = document.getElementById("greeting");
+    greeting.removeAttribute("hidden")
+    hide = document.getElementById("hide_btn");
+    hide.removeAttribute("hidden")
+}
+
+function hide_image() {
+    img = document.getElementById("ksp");
+    img.setAttribute("hidden", null)
+    greeting = document.getElementById("greeting");
+    greeting.setAttribute("hidden", null)
+    hide = document.getElementById("hide_btn");
+    hide.setAttribute("hidden", null)
 }
