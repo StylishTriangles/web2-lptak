cb0efec2cc974c3672f8dd4a7f80d6c8cf0bf2b3
StylishTriangles <hungrycow2012@gmail.com>
2020-02-05 03:11:29 +0100

    Add a little game

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -3,6 +3,35 @@ function hello() {
     console.log("Hello "+x+"!");
 }
 
+let imageX = 200;
+let imageY = 200;
+let imageScore = 0;
+
+function logdiv(message) {
+    let div = document.getElementById("log");
+    div.innerHTML += message + "<br/>";
+}
+
+function update_score() {
+    let score = document.getElementById("score");
+    score.innerHTML=imageScore;
+}
+
+function move_image() {
+    img = document.getElementById("ksp");
+    // imageX = Math.random()*400;
+    // imageY = Math.random()*400;
+    let newStyle = "left: " + (imageX+imageScore) + "px; top: " + imageY + "px;";
+    img.style = newStyle;
+    logdiv("👉\t" + newStyle);
+    imageScore -= 1;
+    update_score();
+}
+
+function enable_movement() {
+    setInterval(move_image, 1000);
+}
+
 function see_image() {
     img = document.getElementById("ksp");
     img.removeAttribute("hidden")
@@ -19,4 +48,9 @@ function hide_image() {
     greeting.setAttribute("hidden", null)
     hide = document.getElementById("hide_btn");
     hide.setAttribute("hidden", null)
+}
+
+function click_image() {
+    imageScore += 1;
+    update_score();
 }
